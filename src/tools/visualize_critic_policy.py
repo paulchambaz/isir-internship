a8d05f9d7c9a75b68778ffb14d92782364f49ea7
Paul Chambaz <paul.chambaz@tutanota.com>
2025-06-20 12:47:23 +0200

    more afu bug fixes

---
+++ src/tools/visualize_critic_policy.py
@@ -0,0 +1,9 @@
+# Copyright (C) 2025 Paul Chambaz
+# This file is part of isir-internship.
+#
+# isir-internship is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+
